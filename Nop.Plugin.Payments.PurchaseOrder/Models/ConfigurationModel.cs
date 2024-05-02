1586502e709f5512ef83cdd9ce3c97f87e4d71b9
Alexey Anokhin <exile.developer@gmail.com>
2024-05-02 09:50:25 +0300

    Changes to work with nopCommerce 4.70

--- Nop.Plugin.Payments.PurchaseOrder/Models/ConfigurationModel.cs
+++ Nop.Plugin.Payments.PurchaseOrder/Models/ConfigurationModel.cs
@@ -1,25 +1,24 @@
 ﻿using Nop.Web.Framework.Models;
 using Nop.Web.Framework.Mvc.ModelBinding;
 
-namespace Nop.Plugin.Payments.PurchaseOrder.Models
+namespace Nop.Plugin.Payments.PurchaseOrder.Models;
+
+public record ConfigurationModel : BaseNopModel
 {
-    public record ConfigurationModel : BaseNopModel
-    {
-        public int ActiveStoreScopeConfiguration { get; set; }
+    public int ActiveStoreScopeConfiguration { get; set; }
 
-        [NopResourceDisplayName("Plugins.Payment.PurchaseOrder.AdditionalFee")]
-        public decimal AdditionalFee { get; set; }
+    [NopResourceDisplayName("Plugins.Payment.PurchaseOrder.AdditionalFee")]
+    public decimal AdditionalFee { get; set; }
 
-        public bool AdditionalFee_OverrideForStore { get; set; }
+    public bool AdditionalFee_OverrideForStore { get; set; }
 
-        [NopResourceDisplayName("Plugins.Payment.PurchaseOrder.AdditionalFeePercentage")]
-        public bool AdditionalFeePercentage { get; set; }
+    [NopResourceDisplayName("Plugins.Payment.PurchaseOrder.AdditionalFeePercentage")]
+    public bool AdditionalFeePercentage { get; set; }
 
-        public bool AdditionalFeePercentage_OverrideForStore { get; set; }
+    public bool AdditionalFeePercentage_OverrideForStore { get; set; }
 
-        [NopResourceDisplayName("Plugins.Payment.PurchaseOrder.ShippableProductRequired")]
-        public bool ShippableProductRequired { get; set; }
+    [NopResourceDisplayName("Plugins.Payment.PurchaseOrder.ShippableProductRequired")]
+    public bool ShippableProductRequired { get; set; }
 
-        public bool ShippableProductRequired_OverrideForStore { get; set; }
-    }
+    public bool ShippableProductRequired_OverrideForStore { get; set; }
 }
--- Nop.Plugin.Payments.PurchaseOrder/Models/PaymentInfoModel.cs
+++ Nop.Plugin.Payments.PurchaseOrder/Models/PaymentInfoModel.cs
@@ -1,11 +1,10 @@
 ﻿using Nop.Web.Framework.Models;
 using Nop.Web.Framework.Mvc.ModelBinding;
 
-namespace Nop.Plugin.Payments.PurchaseOrder.Models
+namespace Nop.Plugin.Payments.PurchaseOrder.Models;
+
+public record PaymentInfoModel : BaseNopModel
 {
-    public record PaymentInfoModel : BaseNopModel
-    {
-        [NopResourceDisplayName("Plugins.Payment.PurchaseOrder.PurchaseOrderNumber")]
-        public string PurchaseOrderNumber { get; set; }
-    }
+    [NopResourceDisplayName("Plugins.Payment.PurchaseOrder.PurchaseOrderNumber")]
+    public string PurchaseOrderNumber { get; set; }
 }
