82df550c32d56b263a4fd4cfe75a4de9d66a2359
John Marshall <jmarshall@connectria.com>
2024-07-10 19:09:25 -0500

    handle client-specific PO requirement on billing step

--- Nop.Plugin.Payments.PurchaseOrder/Components/PaymentPurchaseOrderViewComponent.cs
+++ Nop.Plugin.Payments.PurchaseOrder/Components/PaymentPurchaseOrderViewComponent.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Nop.Plugin.Payments.PurchaseOrder.Models;
 using Nop.Web.Framework.Components;
@@ -14,6 +15,12 @@ public class PaymentPurchaseOrderViewComponent : NopViewComponent
         if (Request.Method != WebRequestMethods.Http.Get)
         {
             model.PurchaseOrderNumber = HttpContext.Request.Form["PurchaseOrderNumber"];
+        }
+
+        //client asked for PO on billing step - if entered pre-fill on this plugin
+        if (string.IsNullOrEmpty(model.PurchaseOrderNumber))
+        {
+            model.PurchaseOrderNumber = HttpContext.Session.GetString("OpcBillingPoNumber");
         }
 
         return View("~/Plugins/Payments.PurchaseOrder/Views/PaymentInfo.cshtml", model);
